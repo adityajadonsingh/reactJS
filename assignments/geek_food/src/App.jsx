f8f949b812db5dc31b6ca83fb92488e25c3e7fd3
aditya-krishi <159108169+aditya-krishi@users.noreply.github.com>
2024-12-20 12:41:11 +0530

    Quotes Page Created & Completed

--- assignments/geek_food/src/App.jsx
+++ assignments/geek_food/src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./containers/Home/Home";
 import "./App.css";
+import Quotes from "./containers/Quotes/Quotes";
 
 function App() {
   return (
     <>
-      <Home/>
+      {/* <Home/> */}
+      <Quotes/>
     </>
   );
 }
