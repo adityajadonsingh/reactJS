3277bc11517cf174d3084ffdce7b2ae58252ac32
aditya-krishi <159108169+aditya-krishi@users.noreply.github.com>
2025-01-03 11:13:29 +0530

    Restaurant Page

--- assignments/geek_food/src/containers/Restaurant/Restaurants.jsx
+++ assignments/geek_food/src/containers/Restaurant/Restaurants.jsx
@@ -3,36 +3,52 @@ import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 
 import RestaurantCard from "./RestaurantCard";
+import data from "../../dataset";
 
-
-
-const Restaurants = (props) => {
-
-    const [resData, setData] = useState(data);
-
+const Restaurants = () => {
+  const [searchText, setSearch] = useState("");
+  const [minRating, setMinRating] = useState(0);
+  const handleSearchText = (e) => {
+    setSearch(e.target.value);
+  };
+  const handleMinRating = (e) => {
+    setMinRating(Number(e.target.value));
+  };
   return (
     <>
       <Header />
       <section className="resturant pt-32 pb-20">
         <div className="container mx-auto">
-          <div className="search-container flex justify-between">
-            <input
-              type="text"
-              placeholder="Search Restaurants"
-              className="border py-1 px-3 focus:outline-none"
-            />
-            <div className="flex items-center gap-x-2">
-              <span className="block">Minimum Rating</span>
+          <form action="">
+            <div className="search-container flex justify-between">
               <input
-                className="border py-1 px-3 w-10 focus:outline-none"
                 type="text"
-                placeholder="4"
+                placeholder="Search Restaurants"
+                className="border py-1 px-3 focus:outline-none"
+                onChange={handleSearchText}
               />
+              <div className="flex items-center gap-x-2">
+                <span className="block">Minimum Rating</span>
+                <input
+                  className="border py-1 px-3 w-16 focus:outline-none"
+                  type="number"
+                  placeholder="4"
+                  onChange={handleMinRating}
+                />
+              </div>
             </div>
-          </div>
+          </form>
           <hr className="my-5" />
           <div className="card-container flex flex-wrap">
-            <RestaurantCard />
+            {
+              data
+              .filter((item) =>
+                (item.name.toLowerCase().includes(searchText.toLowerCase()) || item.address.toLowerCase().includes(searchText.toLowerCase()) ) && item.rating >= minRating
+              )
+              .map((item) => 
+                <RestaurantCard key={item._id} item={item} />
+              )
+            }
           </div>
         </div>
       </section>
