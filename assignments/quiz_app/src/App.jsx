a6b25c41a9c579ddaab765861b564becb2285bb7
Aditya Jadon <51057603+adityajadonsingh@users.noreply.github.com>
2025-01-05 23:36:23 +0530

    Infinite scroll - created

--- assignments/quiz_app/src/App.jsx
+++ assignments/quiz_app/src/App.jsx
@@ -6,11 +6,17 @@ import axios from "axios";
 function App() {
 
   const [questions, setQuestion] = useState([]);
-
+  const [quizStarted, setQuizStarted] = useState(false);
+  const fetchData = async () => {
+    try{
+      const response = await axios.get("https://opentdb.com/api.php?amount=10");
+    console.log(response.data)
+    }catch(error){
+      console.log(error);
+    }
+  }
   useEffect(()=>{
-    const response = axios.get("https://opentdb.com/api.php?amount=10");
-    
-    console.log(response)
+    fetchData();
   }, [])
 
   return (
@@ -20,11 +26,13 @@ function App() {
         <hr className="my-5" />
         <div className="mt-7 w-full flex justify-center">
           <div className="w-3/5 rounded-md border shadow-md py-10 px-5">
-            <QuizCard />
-            <div className="time flex gap-x-3 mt-4">
+            <QuizCard started={quizStarted} setStarted={setQuizStarted} />
+            {
+              quizStarted ? <div className="time flex gap-x-3 mt-4">
               <span className="block font-bold">Time Left:</span>
               <span className="block">10 sec</span>
-            </div>
+            </div> : <></>
+            }
           </div>
         </div>
       </div>
--- assignments/quiz_app/src/QuizCard.jsx
+++ assignments/quiz_app/src/QuizCard.jsx
@@ -1,21 +1,52 @@
-const QuizCard = () => {
+const QuizCard = (props) => {
   return (
     <>
-    <div className="w-full flex gap-x-2">
-            <span className="block font-bold text-nowrap text-xl">Question 1.</span>
-            <span className="block text-xl text-[#575757]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, maxime! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, maxime!</span>
-        </div>
-        <div className="options mt-5">
+      {props.started ? (
+        <>
+          <div className="w-full flex gap-x-2">
+            <span className="block font-bold text-nowrap text-xl">
+              Question 1.
+            </span>
+            <span className="block text-xl text-[#575757]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
+              maxime! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Quo, maxime!
+            </span>
+          </div>
+          <div className="options mt-5">
             <ul className="list-disc ml-10">
-                <li className="my-2"><button className="bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white text-lg font-bold">Option</button></li>
-                <li className="my-2"><button className="bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white text-lg font-bold">Option</button></li>
-                <li className="my-2"><button className="bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white text-lg font-bold">Option</button></li>
-                <li className="my-2"><button className="bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white text-lg font-bold">Option</button></li>
+              <li className="my-2">
+                <button className="bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white text-lg font-bold">
+                  Option
+                </button>
+              </li>
+              <li className="my-2">
+                <button className="bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white text-lg font-bold">
+                  Option
+                </button>
+              </li>
+              <li className="my-2">
+                <button className="bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white text-lg font-bold">
+                  Option
+                </button>
+              </li>
+              <li className="my-2">
+                <button className="bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white text-lg font-bold">
+                  Option
+                </button>
+              </li>
             </ul>
-        </div>
-      
+          </div>
+        </>
+      ) : (
+        <>
+            <div className="flex justify-center items-center h-60 w-full">
+                <button className="text-2xl bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white font-bold" onClick={()=> props.setStarted(true)}>Start Quiz</button>
+            </div>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default QuizCard
+export default QuizCard;
