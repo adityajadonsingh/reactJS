a184c51de7bf3888dc8716afd54e88aee7765c07
aditya-krishi <159108169+aditya-krishi@users.noreply.github.com>
2025-01-07 14:42:53 +0530

    Updated Code - Quiz App

--- assignments/quiz_app/src/App.jsx
+++ assignments/quiz_app/src/App.jsx
@@ -4,21 +4,20 @@ import QuizCard from "./QuizCard";
 import axios from "axios";
 
 function App() {
-
-  const [questions, setQuestion] = useState([]);
+  const [questions, setQuestions] = useState([]);
   const [quizStarted, setQuizStarted] = useState(false);
-  const fetchData = async () => {
-    try{
-      const response = await axios.get("https://opentdb.com/api.php?amount=10");
-    console.log(response.data)
-    }catch(error){
-      console.log(error);
-    }
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10")
+      .then(res => res.json())
+      .then(data => setQuestions(data.results))
+  }, []);
+  const startQuiz = () => {
+    setQuizStarted(!quizStarted);
   }
-  useEffect(()=>{
-    fetchData();
-  }, [])
 
+  console.log(questions);
   return (
     <>
       <div className="container mx-auto my-20">
@@ -26,13 +25,30 @@ function App() {
         <hr className="my-5" />
         <div className="mt-7 w-full flex justify-center">
           <div className="w-3/5 rounded-md border shadow-md py-10 px-5">
-            <QuizCard started={quizStarted} setStarted={setQuizStarted} />
-            {
-              quizStarted ? <div className="time flex gap-x-3 mt-4">
-              <span className="block font-bold">Time Left:</span>
-              <span className="block">10 sec</span>
-            </div> : <></>
-            }
+            
+            {quizStarted && currentQuestion <= questions.length-1  ? (
+              <>
+              <QuizCard questionIdx={currentQuestion} question={questions[currentQuestion]} score={score} setScore={setScore} currentQuestion={currentQuestion} currentQuestionFunc={setCurrentQuestion} />
+              <div className="time flex gap-x-3 mt-4">
+                <span className="block font-bold">Time Left:</span>
+                <span className="block">10 sec</span>
+              </div>
+              </>
+            ) : (
+              currentQuestion <= questions.length-1 ?
+              <>
+               <div className="flex justify-center items-center h-60 w-full">
+                <button className="text-2xl bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white font-bold" onClick={startQuiz}>Start Quiz</button>
+               </div>
+              </>
+              :
+              <>
+                <div className="flex justify-center items-center h-60 w-full flex-col gap-y-4">
+                  <span className="block text-2xl">Your Score</span>
+                <span className="block font-bold text-3xl">{`${score}/${questions.length}`}</span>
+               </div>
+              </>
+            )}
           </div>
         </div>
       </div>
--- assignments/quiz_app/src/QuizCard.jsx
+++ assignments/quiz_app/src/QuizCard.jsx
@@ -1,50 +1,40 @@
 const QuizCard = (props) => {
+  console.log(props.question)
+  const allAnswers = [props.question.correct_answer, ...props.question.incorrect_answers];
+  const shuffleAnswers = allAnswers.sort(() => Math.random() - 0.5);
+  console.log(shuffleAnswers)
+  const checkAnswer = (selectedIdx) => {
+    if(shuffleAnswers[selectedIdx] === props.question.correct_answer){
+      props.setScore(props.score + 1);
+    }
+    props.currentQuestionFunc(props.currentQuestion + 1);
+  }
+  console.log(props.score);
   return (
     <>
-      {props.started ? (
-        <>
-          <div className="w-full flex gap-x-2">
-            <span className="block font-bold text-nowrap text-xl">
-              Question 1.
-            </span>
-            <span className="block text-xl text-[#575757]">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
-              maxime! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Quo, maxime!
-            </span>
-          </div>
-          <div className="options mt-5">
-            <ul className="list-disc ml-10">
-              <li className="my-2">
-                <button className="bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white text-lg font-bold">
-                  Option
-                </button>
-              </li>
-              <li className="my-2">
-                <button className="bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white text-lg font-bold">
-                  Option
-                </button>
-              </li>
-              <li className="my-2">
-                <button className="bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white text-lg font-bold">
-                  Option
-                </button>
-              </li>
-              <li className="my-2">
-                <button className="bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white text-lg font-bold">
-                  Option
-                </button>
-              </li>
-            </ul>
-          </div>
-        </>
-      ) : (
-        <>
-            <div className="flex justify-center items-center h-60 w-full">
-                <button className="text-2xl bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white font-bold" onClick={()=> props.setStarted(true)}>Start Quiz</button>
-            </div>
-        </>
-      )}
+      <div className="w-full flex gap-x-2">
+        <span className="block font-bold text-nowrap text-xl">
+          Question {props.questionIdx + 1}.
+        </span>
+        <span className="block text-xl text-[#575757]">
+          {props.question.question}
+        </span>
+      </div>
+      <div className="options mt-5 flex justify-end">
+        <ul className="list-disc w-5/6">
+        {
+          shuffleAnswers.map((item, idx) => {
+            return <li className="my-2" key={`${idx}-options`}>
+            <button className="bg-[#5e5ee6] hover:bg-[#6a6add] py-2 px-5 rounded text-white text-lg font-bold" onClick={() => checkAnswer(idx)}>
+              {item}
+            </button>
+          </li>
+          })
+        }
+          
+          
+        </ul>
+      </div>
     </>
   );
 };
